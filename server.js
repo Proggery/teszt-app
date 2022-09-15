8a5aa99999483453847a4426ff4e4852e5fe1a76
Proggery <80548375+Proggery@users.noreply.github.com>
2022-09-15 10:40:54 +0200

    ads

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port = process.env.PORT || 5555
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`A szerver fut: http://localhost:${port}`);
+});
