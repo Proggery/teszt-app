1733891bd237eb6379c598b25a2a19eb1b982510
Proggery <80548375+Proggery@users.noreply.github.com>
2022-09-15 10:45:24 +0200

    asd

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
+app.use(cors())
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 5555
