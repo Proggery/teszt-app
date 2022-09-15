afd5398c25a4ec3e21fc37193bc1521bb674135c
Proggery <80548375+Proggery@users.noreply.github.com>
2022-09-16 01:16:04 +0200

    asd

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.resolve(__dirname, "client/build")));
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
