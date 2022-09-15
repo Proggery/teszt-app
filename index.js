20e9228353104c9e88c523b8421debd2e0859e0b
Proggery <80548375+Proggery@users.noreply.github.com>
2022-09-16 01:41:09 +0200

    asd

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 // import mongoose from "mongoose";
 // import dotenv from "dotenv";
 // import cookieParser from "cookie-parser"
@@ -16,6 +18,9 @@ const port = process.env.PORT || 5555;
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // dotenv.config();
 
