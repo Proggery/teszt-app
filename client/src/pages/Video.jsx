53fd7d95003cee189020f5340484a4f113a802a2
Proggery <80548375+Proggery@users.noreply.github.com>
2022-09-17 13:22:34 +0200

    asd

--- client/src/pages/Video.jsx
+++ client/src/pages/Video.jsx
@@ -7,107 +7,10 @@ import {
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 import { API_URL } from "../api/api";
 import Comments from "../components/Comments";
 import { useSelector } from "react-redux";
-
-const Container = styled.div`
-  display: flex;
-  gap: 24px;
-`;
-
-const Content = styled.div`
-  flex: 5;
-`;
-const VideoWrapper = styled.div``;
-
-const Title = styled.h1`
-  font-size: 18px;
-  font-weight: 400;
-  margin-top: 20px;
-  margin-bottom: 10px;
-  color: ${({ theme }) => theme.text};
-`;
-
-const Details = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const Info = styled.span`
-  color: ${({ theme }) => theme.textSoft};
-`;
-
-const Buttons = styled.div`
-  display: flex;
-  gap: 20px;
-  color: ${({ theme }) => theme.text};
-`;
-
-const Button = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 5px;
-  cursor: pointer;
-`;
-
-const Hr = styled.hr`
-  margin: 15px 0px;
-  border: 0.5px solid ${({ theme }) => theme.soft};
-`;
-
-const Recommendation = styled.div`
-  flex: 2;
-`;
-const Channel = styled.div`
-  display: flex;
-  justify-content: space-between;
-`;
-
-const ChannelInfo = styled.div`
-  display: flex;
-  gap: 20px;
-`;
-
-const Image = styled.img`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-`;
-
-const ChannelDetail = styled.div`
-  display: flex;
-  flex-direction: column;
-  color: ${({ theme }) => theme.text};
-`;
-
-const ChannelName = styled.span`
-  font-weight: 500;
-`;
-
-const ChannelCounter = styled.span`
-  margin-top: 5px;
-  margin-bottom: 20px;
-  color: ${({ theme }) => theme.textSoft};
-  font-size: 12px;
-`;
-
-const Description = styled.p`
-  font-size: 14px;
-`;
-
-const Subscribe = styled.button`
-  background-color: #cc1a00;
-  font-weight: 500;
-  color: white;
-  border: none;
-  border-radius: 3px;
-  height: max-content;
-  padding: 10px 20px;
-  cursor: pointer;
-`;
+import { Button, Buttons, Channel, ChannelCounter, ChannelDetail, ChannelInfo, ChannelName, Container, Content, Description, Details, Hr, Image, Info, Subscribe, Title, VideoWrapper } from "./Video.styled";
 
 const Video = () => {
   const params = useParams();
@@ -118,8 +21,6 @@ const Video = () => {
   const [channel, setChannel] = useState({});
   const [isSub, setIsSub] = useState(true);
   const [isUser, setIsUser] = useState(true);
-
-  console.log(currentUser);
 
   useEffect(() => {
     const fetchVideo = async () => {
@@ -141,7 +42,7 @@ const Video = () => {
 
   useEffect(() => {
     if (video.userId !== undefined) {
-      if (currentUser._id !== video.userId) {
+      if (currentUser && currentUser._id !== video.userId) {
         setIsUser(false);
       }
     } else {
@@ -203,7 +104,7 @@ const Video = () => {
                     : video.dislikes}
                 </Button>
               </>
-            )}{" "}
+            )}
             <Button>
               <ReplyOutlined /> Share
             </Button>
